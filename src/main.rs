2b2d24ae2f10ff344a97516a3d4df008743d604e
jvtubergen <jvtubergen@pm.me>
2023-09-25 16:59:36 +0200

    Renderer resizes with window.

--- src/main.rs
+++ src/main.rs
@@ -47,26 +47,35 @@ impl eframe::App for MyApp {
         if !self.init {
 
             // Initiate texture.
-            self.image_id = Some(ctx.tex_manager().write().alloc("renderer".to_owned(), self.image_delta.image.clone(), self.image_delta.options));
+            self.image_id = Some(
+                ctx.tex_manager().write().alloc(
+                    "renderer".to_owned(), 
+                    self.image_delta.image.clone(), 
+                    self.image_delta.options
+                )
+            );
         }
 
+        let size = ctx.screen_rect().size();
+        let w = size.x as usize;
+        let h = size.y as usize;
 
         // Create pixels.
         let mut pixels = vec![];
-        pixels.resize(500*500, Color32::from_rgb(0, 0, 0));
+        pixels.resize(h*w, Color32::from_rgb(0, 0, 0));
 
 
         // Render white box at mouse position.
         if let Some(pos) = ctx.input(|i| i.pointer.hover_pos()) {
             let y_min = 0.max((pos.y - 5.) as usize);
-            let y_max = 500.min((pos.y + 5.) as usize);
+            let y_max = h.min((pos.y + 5.) as usize);
 
             let x_min = 0.max((pos.x - 5.) as usize);
-            let x_max = 500.min((pos.x + 5.) as usize);
+            let x_max = w.min((pos.x + 5.) as usize);
 
             for y in y_min..y_max {
                 for x in x_min..x_max {
-                    pixels[500*y+x] = Color32::from_rgb(
+                    pixels[w*y+x] = Color32::from_rgb(
                         255,255,255
                     );
                 }
@@ -75,7 +84,7 @@ impl eframe::App for MyApp {
 
         self.image_delta.image = ImageData::Color(ColorImage {
             pixels, 
-            size: [500, 500]
+            size: [w, h]
         });
 
 
@@ -87,7 +96,7 @@ impl eframe::App for MyApp {
         egui::Area::new("renderer")
             .fixed_pos(egui::pos2(0.0, 0.0))
             .show(ctx, |ui| {
-                ui.image(id, Vec2 { x: 500., y: 500. });
+                ui.image(id, Vec2 { x: w as f32, y: h as f32 });
             });
 
     }
